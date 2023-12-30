cad661469457cc27fa4014fa49014191b7e0a4b7
fiskOfficial <132220682+fiskOfficial@users.noreply.github.com>
2023-12-30 20:28:55 +0200

    mainPage2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import noamsLogo from './noamsLogo.png'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navigation'>
+      <div className='leftNav'>
+          <div className='shoppingBagIconC'><img alt='shopping bag icon' className='shoppingBagIcon' src='https://static.vecteezy.com/system/resources/previews/010/576/761/original/shopping-bag-icon-logo-illustration-grocery-bag-symbol-template-for-graphic-and-web-design-collection-free-vector.jpg'/></div>
+          <div>קצת עליי</div>
+          <div>אני אחלה חמודה</div>
+          <div>אוהבת עיצוב</div>
+          <div>את אחלה</div>
+          <div>ואת חמודה</div>
+          <div>יאללה יאלהה</div>
+          </div>
+        <img className='mainLogoHomeBtn' src={noamsLogo} alt='Main Logo'/>
+      </div>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Welcome.css"
+
+function Welcome() {
+  return (
+    <div className='welcomeCompounent'>
+    <div className='welcomeGreeting'><div className='welcomeanimation'>welcome</div></div>
+    <div className='mainPicture'></div>
+    </div>
+  )
+}
+
+export default Welcome
